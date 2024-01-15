e4b5b3be134f6e25478b7aa1f2c226406d164218
quielLovesLasagna <exequielarco23@gmail.com>
2024-01-15 22:20:46 +0800

    Added email validation functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+// ELEMENT/S
+const inputEl = document.querySelector(".header__input");
+const inputContainerEl = document.querySelector(".header__input-container");
+const submitBtnEl = document.querySelector(".header__button");
+
+// FUNCTION/S
+function addError() {
+	inputContainerEl.classList.add("error");
+}
+
+function removeError() {
+	inputContainerEl.classList.remove("error");
+}
+
+function validateInput(email) {
+	// -- Email validation regex
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	// -- Testing the email, returns true if valid. Else, false
+	return emailRegex.test(email);
+}
+
+function handleValdation() {
+	const isValid = validateInput(inputEl.value);
+
+	if (!isValid) {
+		addError();
+	} else {
+		removeError();
+	}
+}
+
+// EVENT LISTENER/S
+submitBtnEl.addEventListener("click", handleValdation);
+
+inputEl.addEventListener("keyup", (e) => {
+	if (e.key === "Enter") {
+		handleValdation();
+	}
+});
